0c764a321f80f14e7563ae84d68bfc33151439e9
Rae Gebhart <raegebhart@gmail.com>
2023-02-21 11:57:44 -0700

    Pass deleteResy down as a prop

--- src/component/Reservations/Reservations.js
+++ src/component/Reservations/Reservations.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './Reservations.css'
 import Reservation from '../Reservation/Reservation'
 
-const Reservations = ({ reservations }) => {
-  const allReservations = reservations.map(reso => <Reservation id={reso.id} key={reso.id} name={reso.name} date={reso.date} time={reso.time} number={reso.number} />)
+const Reservations = ({ reservations, deleteResy }) => {
+  const allReservations = reservations.map(reso => <Reservation id={reso.id} key={reso.id} name={reso.name} date={reso.date} time={reso.time} number={reso.number} deleteResy={deleteResy} />)
 
   return(
     <section className='resy-container' >
