a51afef84e64c6c53cc0f866a34cf8200e45d3d0
Rae Gebhart <raegebhart@gmail.com>
2023-02-21 11:57:22 -0700

    Add onClick to cancel button

--- src/component/Reservation/Reservation.js
+++ src/component/Reservation/Reservation.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Reservation.css'
 
-const Reservation = ({ id, name, date, time, number }) => {
+const Reservation = ({ id, name, date, time, number, deleteResy }) => {
   return (
     <div className='reservation'>
       <h3>{name}</h3>
       <p>{date}</p>
       <p>{time} pm</p>
       <p>Number of guests: {number}</p>
-      <button className='cancel-button' >Cancel</button>
+      <button onClick={() => deleteResy(id)} className='cancel-button' >Cancel</button>
     </div>
   )
 }
